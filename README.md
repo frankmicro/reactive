1595275bce2dee63f3ffc9d48b355535bb32daf4
Rohan Parkar <rohan.parkar@kissht.com>
2023-02-17 17:02:54 +0530

    component basics

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-#install react, install react-dom
-#jsx is html like syntax but not html
-#tab-index will be tabIndex in jsx
-#class will be className
-#babel converts jsx to normal html and js in a compilation way
-#babel comes along with bundlers dont have to install seperate
+#React Components
+#functional - NEw
+#class based - old
+#keep component names in capital to maintain uniformity
+#use a component inside a component is component composition
--- app.js
+++ app.js
@@ -1,36 +1,26 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-// REACT IDEATION TO WRITE HTML INSIDE JS
-// createElement was not efficient ..so jsx came in picture
-// const heading = document.createElement('h1')
-// heading.innerHTML = 'Hello world!'
-// const root = document.getElementById('root')
-// root.appendChild(heading)
-// React is global variable
-// react element is an object
-// there will always be one root, anything inside this will be overridden
-//react is core package
-// react dom to manipulate dom
-const heading = React.createElement('h1', { //type or name of tag
-    //class: 'test' //attribute
-    "id":"h1",
-    "key":"h1"
-}, 'Hello React!')//text
-//core library will always be there
-const header2 = (
-    <h2 id="h2" key="h2">
-        Header2
-    </h2>
+
+const header2 = (//react element
+    <h3 id="h2" key="h2">
+        Header 3 i am an element
+    </h3>
 ) 
-const header3 = React.createElement('h3', {
-    "id":"h3",
-    "key":"h3"
-}, 'Header 3')
-//base container
-const container = React.createElement('div', {
-    id : 'container'
-}, [heading, header2, header3])
-const rootId = document.getElementById('root')
-const root = ReactDOM.createRoot(rootId) //second lib in picture
+const Title = () => {//can also be called if function
+    return (
+        <h3>Food Villa</h3>
+    )
+}
+const HeaderComponent = () => (//function component
+        <div>
+            {header2}
+            {Title()}
+            <Title/>
+            <h1>H1 tag here..</h1>
+            <h2>H2 tag here..</h2>
+        </div>
+    );
+
+const root = ReactDOM.createRoot(document.getElementById('root')) //second lib in picture
 //passing react element inside root
-root.render(container)//heading will give us document object
+root.render(<HeaderComponent/>)//heading will give us document object
